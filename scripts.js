47aac6615a52792156cabb5c51b570f21f902432
varshakanugula <varshakanugula17@gmail.com>
2023-11-27 17:28:34 +1100

    ,
    .

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
 const cardList = [
     {
         title: "Kitten 2",
-        image: "images/kitten2.png",
+        image: "images/kitty 2.jpg",
         link: "About Kitten 2",
         description: "Demo description about kitten 2"
     },
     {
         title: "Kitten 3",
-        image: "images/kitten3.png",
+        image: "images/kitty 3.jpg",
         link: "About Kitten 3",
-        description: "Demo description about kitten 3"
+        description: " hello "
     }
 ];
 
